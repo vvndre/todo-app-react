f4d27d1604a82dde35c044dc387c552e2b31311c
andrercastillon <andrercastillon@gmail.com>
2023-07-28 15:23:09 -0500

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  //new app
+  constructor(props) {
+    // React.Component. contstructor(props);
+    super(props);
+    this.state = {
+      //isClicked: false,
+      todos: [],
+      text: "",
+    };
+    this.onClickHandler = () => {
+      // get the input text
+      const text = this.state.text;
+      // create a new list of the current todos,
+      // add the new text
+      const todos = [...this.state.todos, text];
+      // Set the new todo list to the state
+      this.setState({
+        todos: todos,
+        text: "",
+        // isClicked: !this.state.isClicked
+      });
+    };
+
+    this.onChangeHandler = (evt) => {
+      this.setState({
+        text: evt.target.value,
+      });
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World!
+        <hr />
+        <button onClick={this.onClickHandler}> Add Item </button>
+        {/* <p>Am I Clicked? {this.state.isClicked ? 'Yes' : 'No'}</p>  */}
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={this.onChangeHandler}
+        />
+        {/* <p>Text State?
+          {this.state.text}
+        </p> */}
+        <h3> Todo List </h3>
+        <ul>
+          {this.state.todos.map((todo, index) => {
+            return <Todo key={index}>{todo}</Todo>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+class Todo extends React.Component {
+  render() {
+    return (
+      <li className="Todo">
+        <input type="checkbox" /> {this.props.children}
+      </li>
+    );
+  }
+}
